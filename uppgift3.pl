6b3dd33d73257f8386fed04afd3aecd258eef26e
Mostafa Faik <mostafa.faik07@gmail.com>
2025-09-29 20:29:31 +0200

    Finished Task3

---
+++ uppgift3.pl
@@ -0,0 +1,12 @@
+% Simple length predicate (if you don't already have my_length/2)
+my_length([], 0).
+my_length([_|T], N) :-
+    my_length(T, N1),
+    N is N1 + 1.
+
+% Main predicate: partstring(List, Length, Substring)
+partstring(List, L, F) :-
+    append(_, Rest, List),   % pick a suffix Rest of List
+    append(F, _, Rest),      % pick a prefix F of Rest -> contiguous
+    F \= [],                 % exclude empty substring
+    my_length(F, L).         % compute length of F
