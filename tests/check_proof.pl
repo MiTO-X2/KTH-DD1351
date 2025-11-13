626b87f617a87d1b221b7097f9763b149f7335ee
Mostafa Faik <mfaik@kth.se>
2025-11-13 21:25:52 +0100

    Wrote some logical rules predicates

--- tests/check_proof.pl
+++ tests/check_proof.pl
@@ -37,3 +37,29 @@ valid_rule([[Line, Formula, assumption] | Box], Prems, Previous) :-
 % Get the formula from a given line number
 get_formula(LineNum, Previous, Formula) :-
     member([LineNum, Formula, _], Previous), !.
+
+
+% --- Logical Rules ---
+
+% Copy a formula from a previous line in the proof
+valid_rule([_, Formula, copy(X)], _, Previous) :-
+    get_formula(X, Previous, Formula).
+
+
+% And introduction rule (∧i x, y)
+% If from x we have A, and from y we have B, we can conclude and(A,B)
+valid_rule([_, and(A,B), andint(X,Y)], _, Previous) :-
+    get_formula(X, Previous, A),
+    get_formula(Y, Previous, B).
+
+
+% And elimination rules (∧e1 x)
+% If from line x we have and(A,_), we can conclude A
+valid_rule([_, A, andel1(X)], _, Previous) :-
+    get_formula(X, Previous, and(A,_)).
+
+
+% And elimination rules (∧e2 x)
+% If from line x we have and(_,B), we can conclude B.
+valid_rule([_, B, andel2(X)], _, Previous) :-
+    get_formula(X, Previous, and(_,B)).
